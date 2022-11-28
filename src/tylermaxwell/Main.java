fb18b74bced6df32cf45dd83b506fea0182d2a8d
tmax818 <tylermaxwell661@gmail.com>
2022-11-28 13:49:17 -0800

    remove solution

--- src/tylermaxwell/Main.java
+++ src/tylermaxwell/Main.java
@@ -1,14 +1,8 @@
 package tylermaxwell;
 
 public class Main {
-	
-	public static String name = "Coding Dojo";
 
-	public static void main(String[] args) {
-		System.out.println("My name is " + name);
-		System.out.println("I am 100 years old");
-		System.out.println("My hometown is Burbank, CA");
 
-	}
+
 
 }
