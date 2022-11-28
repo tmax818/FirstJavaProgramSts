548f86ccef1b503ad65b81799a18120005402c1a
tmax818 <tylermaxwell661@gmail.com>
2022-11-28 13:40:20 -0800

    update with new name

--- src/tylermaxwell/Main.java
+++ src/tylermaxwell/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("My name is Coding Dojo");
 		System.out.println("I am 100 years old");
+		System.out.println("My hometown is Burbank, CA");
 
 	}
 
