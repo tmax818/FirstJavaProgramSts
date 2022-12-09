d42e461131a97789949c182afe62a536c25b5cdb
tmax818 <tylermaxwell661@gmail.com>
2022-12-09 14:25:07 -0800

    update solution

--- src/tylermaxwell/Main.java
+++ src/tylermaxwell/Main.java
@@ -3,10 +3,7 @@ package tylermaxwell;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("My name is Coding Dojo");
-		System.out.println("I am 100 years old");
-		System.out.println("My hometown is Burbank, CA");
-
+        System.out.println("My name is Coding Dojo\nI am 100 years old\nMy hometown is Burbank CA");
 	}
 
 }
